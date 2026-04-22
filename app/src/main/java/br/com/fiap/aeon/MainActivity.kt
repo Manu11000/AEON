227b17cbfc9fa81d032376809833e3429b86bda5
Paula Carregal Coelho <paula@MacBook-Air-de-Paula.local>
2026-04-22 17:44:04 -0300

    Alteracao no codigo

--- app/src/main/java/br/com/fiap/aeon/MainActivity.kt
+++ app/src/main/java/br/com/fiap/aeon/MainActivity.kt
@@ -11,33 +11,30 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import br.com.fiap.aeon.screens.`LoginScreen.kt`
+import br.com.fiap.aeon.screens.LoginScreen
 import br.com.fiap.aeon.screens.MenuScreen
-import br.com.fiap.aeon.ui.theme.AEONTheme
+import br.com.fiap.aeon.ui.theme.AEON2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AEONTheme {
+            AEON2Theme {
+                // O navController deve ser declarado AQUI, fora do Scaffold
                 val navController = rememberNavController()
 
-                // O Scaffold dá o espaço para o conteúdo não bater na barra do sistema
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val navController = rememberNavController()
-
                     NavHost(
                         navController = navController,
-                        startDestination = "login"
+                        startDestination = "login",
+                        modifier = Modifier.padding(innerPadding) // Aplica o padding aqui
                     ) {
                         composable("login") {
-                            // Aqui você passa o motor para a tela de Login
-                            `LoginScreen.kt`(modifier = Modifier.padding(innerPadding), navController)
+                            LoginScreen(navController = navController)
                         }
                         composable("menu") {
-                            // Aqui você passa o motor para a tela de Menu (Feed)
-                            MenuScreen(modifier = Modifier.padding(innerPadding), navController)
+                            MenuScreen(navController = navController)
                         }
                     }
                 }
